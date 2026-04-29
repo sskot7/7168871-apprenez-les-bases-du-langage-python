8d18c238c90857ea087dc0c5ae34487eb560e03c
sskot7 <salma.kontar1988@gmail.com>
2026-04-29 16:26:18 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,15 @@
 # Écrivez votre code ici !
+nom = "salma"
+age = 17
+taille = 1.72
+est_etudiant = True
+
+print(nom)
+print(age)
+print(taille)
+print(est_etudiant)
+
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
