d4084166da5448a05e09e1ea72b5ded101b9bfa8
sskot7 <salma.kontar1988@gmail.com>
2026-04-29 18:04:01 +0200

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,12 @@
 # Écrivez votre code ici !
+liste = ["pomme", "banane", "orange"]
+print(liste)
+
+liste.append("kiwi")
+liste.remove("orange")
+
+liste[1] = "ananas"
+
+len(liste)
+
+print(liste.sorted)
